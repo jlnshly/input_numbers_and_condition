2b57ec5c5bdb8ebdc3391f1bbb3a23d98bc4d917
jlnshly <jelaineashleyramos@gmail.com>
2026-03-12 02:47:41 +0800

    code is working. only shows result when input is invalid

---
+++ print_lowest_to_highest.py
@@ -0,0 +1,14 @@
+#Ask user input
+#Set a loop
+#Set the conditions to find lowest number
+number_list = []
+while True:
+    try:
+        user_input = float(input("Enter a number: "))
+        number_list.append(user_input)
+    except ValueError:
+        break
+if number_list:
+    sorted_list = sorted(number_list)
+print(sorted_list)
+
