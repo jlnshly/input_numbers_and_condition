ff3c8b8246520cf819116b508d5d3da11af3add6
jlnshly <jelaineashleyramos@gmail.com>
2026-03-12 02:41:08 +0800

    Code fixed

--- print_unique_or_duplicate.py
+++ print_unique_or_duplicate.py
@@ -3,9 +3,12 @@
 #Finalize conditions
 number_list = []
 while True:
-    user_input = float(input("Enter a number: "))
-    if user_input in number_list:
-        print("Duplicate")
-    else:
-        print("Unique")
-    number_list.append(user_input)
+    try:
+        user_input = float(input("Enter a number: "))
+        if user_input in number_list:
+            print("Duplicate")
+        else:
+            print("Unique")
+        number_list.append(user_input)
+    except ValueError:
+        break
